7bac46417687cc8beab7c7769c80511f3f87d511
NafisFuadTanvir <nafisf026@gmail.com>
2025-02-24 12:06:24 +0600

    create patient doctor and appointment model

--- clinic_management/models.py
+++ clinic_management/models.py
@@ -9,3 +9,21 @@ class User(AbstractUser):
         ('patient', 'Patient'),
     ]
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='patient')
+    
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=255)
+    
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    contact_number = models.CharField(max_length=255)
+    medical_history = models.TextField()  
+     
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    appointment_time = models.TimeField(unique=True)
